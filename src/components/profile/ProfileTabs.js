1d36a84d40917504b409442595375a3f2af61866
flowersayo <flowersayo0811@ewhain.net>
2022-05-08 21:15:34 +0900

    <feat> 모든 프로필 탭 인터랙션 추가

--- src/components/profile/ProfileTabs.js
+++ src/components/profile/ProfileTabs.js
@@ -19,6 +19,11 @@ function TabPanel(props) {
       id={`simple-tabpanel-${index}`}
       aria-labelledby={`simple-tab-${index}`}
       {...other}
+      style={{
+        display: "flex",
+        justifyContent: "column",
+        alignItems: "center",
+      }}
     >
       {value === index && (
         <Box sx={{ p: 3 }}>
@@ -49,6 +54,26 @@ export default function ProfileTabs() {
     setValue(newValue);
   };
 
+  const my_feed = (
+    <FlipMove>
+      {dummy.map((post) => (
+        <Post
+          key={post.text}
+          displayName={post.displayName}
+          username={post.username}
+          verified={post.verified}
+          text={post.text}
+          avatar={post.avatar}
+          image={post.image}
+          tag={post.tag}
+          like={post.like}
+          comment={post.comment}
+          retweet={post.retweet}
+        />
+      ))}
+    </FlipMove>
+  );
+
   return (
     <Box sx={{ width: "100%" }}>
       <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
@@ -68,32 +93,27 @@ export default function ProfileTabs() {
         </Tabs>
       </Box>
       <TabPanel value={value} index={0}>
-        <FlipMove>
-          {dummy.map((post) => (
-            <Post
-              key={post.text}
-              displayName={post.displayName}
-              username={post.username}
-              verified={post.verified}
-              text={post.text}
-              avatar={post.avatar}
-              image={post.image}
-              tag={post.tag}
-              like={post.like}
-              comment={post.comment}
-              retweet={post.retweet}
-            />
-          ))}
-        </FlipMove>
+        {my_feed}
       </TabPanel>
       <TabPanel value={value} index={1}>
-        트윗 및 답글
+        {my_feed}
       </TabPanel>
       <TabPanel value={value} index={2}>
-        미디어
+        <img
+          alt=""
+          draggable="true"
+          id="no_media"
+          src="https://abs.twimg.com/sticky/illustrations/empty-states/masked-doll-head-with-camera-800x400.v1.png"
+        />
+        <h1>조명, 카메라... 첨부!</h1>
+        <p>사진이나 동영상을 포함한 트윗을 전송하면 여기에 표시됩니다.</p>
       </TabPanel>
       <TabPanel value={value} index={3}>
-        마음에 들어요
+        <h1>아직 마음에 들어한 트윗이 없습니다.</h1>
+        <p>
+          좋아하는 트윗의 하트를 눌러 마음에 들어요 표시를 해보세요. 마음에
+          들어한 트윗은 여기에 표시됩니다.
+        </p>
       </TabPanel>
     </Box>
   );
